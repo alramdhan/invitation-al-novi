7d945e2203870e12d5c91e9edc02abbf9b6da799
alramdhan <al.ramdhan312@gmail.com>
2024-05-05 15:07:28 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
+import AOS from 'aos';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'aos/dist/aos.css';
 import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom'
+import { Component } from 'react';
+import WelcomeModal from './Welcome.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/invitation"
+          element={<InvitationPage />} />
+      </Routes>
+    </BrowserRouter>
   );
+}
+
+class InvitationPage extends Component {
+  componentDidMount() {
+    AOS.init();
+  }
+
+  render() {
+    return (
+      <div className='App' style={{float: 'left'}}>
+        <title>Undangan Pernikahan Al dan Novi</title>
+        <nav className='navbar bg-dark navbar-expand fixed-bottom rounded-top-4 p-0" id="navbar-menu'>
+          <ul className="navbar-nav nav-justified w-100 align-items-center">
+            <li className="nav-item">
+                <a className="nav-link" href="#home">
+                    <i className="fa-solid fa-house"></i>
+                    <span className="d-block" style={{fontSize: 0.7 + "rem"}}>Home</span>
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#mempelai">
+                    <i className="fa-solid fa-user-group"></i>
+                    <span className="d-block" style={{fontSize: 0.7 + "rem"}}>Mempelai</span>
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#tanggal">
+                    <i className="fa-solid fa-calendar-check"></i>
+                    <span className="d-block" style={{fontSize: 0.7 + "rem"}}>Tanggal</span>
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#galeri">
+                    <i className="fa-solid fa-images"></i>
+                    <span className="d-block" style={{fontSize: 0.7 + "rem"}}>Galeri</span>
+                </a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#ucapan">
+                    <i className="fa-solid fa-comments"></i>
+                    <span className="d-block" style={{fontSize: 0.7 + "rem"}}>Ucapan</span>
+                </a>
+            </li>
+          </ul>
+        </nav>
+
+        <main className='container bg-dark text-light' data-bs-spy="scroll" data-bs-target="#navbar-menu" data-bs-smooth-scroll="true">
+          <section className='container' id='home'>
+            <div className="text-center pt-4">
+              <h1 className="font-esthetic my-4" style={{fontSize: 2.5+"rem"}}>Undangan Pernikahan</h1>
+
+              <div className="py-4">
+                <div className="img-crop border border-3 border-light shadow mx-auto">
+                  <img width={150} height={150} src="" alt="bg" />
+                </div>
+              </div>
+
+              <h1 className="font-esthetic my-4" style={{fontSize: 3+"rem"}}>Al & Novi</h1>
+              <p className="mb-0" style={{fontSize: 1.5+"rem"}}>Minggu, 23 Juni 2024</p>
+
+              <a className="btn btn-outline-light btn-sm shadow rounded-pill px-3 my-2" target="_blank" rel='noreferrer' href="https://calendar.google.com/calendar/render?action=TEMPLATE&text=The%20Wedding%20of%20Wahyu%20and%20Riski&details=The%20Wedding%20of%20Wahyu%20and%20Riski%20%7C%2015%20Maret%202023%20%7C%20RT%2010%20RW%2002,%20Desa%20Pajerukan,%20Kec.%20Kalibagor,%20Kab.%20Banyumas,%20Jawa%20Tengah%2053191%20%7C%2010.00%20-%2011.00%20WIB&dates=20230315T100000/20230315T110000&location=https://goo.gl/maps/ALZR6FJZU3kxVwN86">
+                <i className="fa-solid fa-calendar-check me-2"></i>Save The Date
+              </a>
+
+              <div className="d-flex justify-content-center align-items-center mt-4 mb-2">
+                <div className="mouse-animation border border-2">
+                  <div className="scroll-animation"></div>
+                </div>
+              </div>
+
+              <p className="m-0">Scroll Down</p>
+            </div>
+          </section>
+        </main>
+
+        <WelcomeModal />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Welcome.js
@@ -0,0 +1,67 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
+import { useSearchParams } from 'react-router-dom';
+import './App.css';
+
+function WelcomeModal() {
+    const [queryParam] = useSearchParams();
+    const [show, setShow] = useState(true);
+    const handleClose = () => setShow(false);
+    // const queryParam = new URLSearchParams(window.location.search);
+    // const name = queryParam.get("to");
+    var [modalClose, setModalClose] = useState("");
+
+    const onClick = () => {
+        setModalClose("tutup-welcome");
+        setInterval(() => {
+            setShow(false);
+        }, 3000);
+    };
+
+    return(
+        <>
+            <Modal
+                show={show}
+                onHide={handleClose}
+                backdrop={"static"}
+                fullscreen={true}
+                keyboard={false}
+                centered={true}
+                scrollable={false}
+                className={modalClose}
+            >
+                <Modal.Body className={"bg-black"}>
+                    <div id="welcome" style={{opacity: 1, paddingTop: 12}}>
+                        <div className="d-flex justify-content-center align-items-center">
+                            <div className="text-center">
+                                <h1 className="font-esthetic mb-4" style={{fontSize: 3.2+"rem"}}>The Wedding Of</h1>
+
+                                <div className="col-12 img-crop border border-3 border-light mb-4 mx-auto rounded-circle" style={{
+                                    width: 64+"%",
+                                    height: 190,
+                                    boxShadow: "0px 10px 20px 0px rgba(232,232,232,0.5)",
+                                    background: "url('https://www.brides.com/thmb/wb0WW5OfI6IDrX3Rm0oMS3l8Jt4=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/wedding-planning-ultimate-guide-recirc-Janet-Lin-Photography-f3d7c8f9fe854f69ada0fc53e6d100bd.jpg')",
+                                    backgroundSize: "cover"
+                                }}></div>
+
+                                <h1 className="font-esthetic my-4" style={{fontSize: 2.5+"rem"}}>Al &amp; Novi</h1>
+                                <div style={{height: 50}}></div>
+                                <div id="guest-name" style={{color: "#E2E2EF"}}>Kepada Yth Bapak/Ibu/Saudara/i</div>
+                                <h3 className='mb-3' style={{color: "#E2E2EF"}}>{queryParam.get("to")}</h3>
+                                <div style={{height: 20}}></div>
+                                <Button variant="light" onClick={onClick} className="rounded-pill">
+                                    <FontAwesomeIcon icon={faEnvelope} />&nbsp;Buka Undangan
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </>
+    )
+}
+
+export default WelcomeModal;
