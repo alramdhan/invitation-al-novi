3f5e351f5e303c4b87013e14b251248aa9188a69
alramdhan <al.ramdhan312@gmail.com>
2024-05-15 22:42:42 +0700

    fix route

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 const router = createBrowserRouter([
   {
-    path: "/wedding-invitation/al-novi",
+    path: "/wedding-invitation/al-novi/",
     element: <App />
   }
 ]);
